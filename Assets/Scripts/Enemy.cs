d4f99a4d91122fc2dd460d37394042174e00392b
ll-hotel <ll-hotel@student.42lyon.fr>
2025-03-17 14:12:20 +0100

    Add Enemy Circle

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Enemy : MonoBehaviour
+{
+	public float            hp = 10;
+	public TextMeshProUGUI  txt;
+
+	void	Start()
+	{
+		DisplayText();
+	}
+	void	Update()
+	{
+		
+	}
+	void	TakeDamage(float dmg)
+	{
+		if (dmg > hp)
+			hp = 0f;
+		else
+			hp -= dmg;
+		DisplayText();
+	}
+
+	void	DisplayText()
+	{
+		txt.text = "HP:" + hp.ToString();
+	}
+
+}
