d59143eda630b201ca229febb0f2e99aa3509bcb
Dracian <71342499+Dracian-Tanaki@users.noreply.github.com>
2025-03-21 02:22:49 +0100

    go avancer la

--- Assets/DisplayTowerInventory.cs
+++ Assets/DisplayTowerInventory.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class DisplayTowerInventory : MonoBehaviour
 {
     private Inventory   inventory;
     private GameObject  inventoryWindow;
+    private Sprite      test;
 
     void Start()
     {
         // inventory = GetComponent<Tower>().inventory;
         inventoryWindow =  GameObject.FindWithTag("TowerInventory");
-        inventoryWindow.transform.Find("TowerPreview").GetComponent<Image>();
+        inventoryWindow.transform.Find("TowerPreview").GetComponent<Image>().sprite = test;
+
     }
 }
